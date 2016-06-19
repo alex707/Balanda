6311a9b1822a84acd11e75275bf02308a8a2882e
alex707 <alex.grigorenko2942@gmail.com>
2016-06-19 04:46:44 +0300

    added buttoms but not set

---
+++ README.md
@@ -0,0 +1,9 @@
+it can read
+Импорт товаров и услуг
+	"parameters":[{"name":"year","default":"2015"},{"name":"quarter","default":"2"},{"name":"kind","default":"в текущих ценах"}]}
+Валовой внутрений продукт
+	"parameters":[{"name":"year","default":"2015"}]},
+Прямые инвестиции (активы)
+	"parameters":[{"name":"year","default":"2016"}]},
+Активы Российской Федерации (международный платежный баланс)
+	"parameters":[{"name":"year","default":"2016"}]},
--- kotyata.rb
+++ kotyata.rb
@@ -21,125 +21,113 @@ ord2_params = []
 
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
-		
+
+		  case message.text
+  when '/start'
+    question = 'London is a capital of which country?'
+    answers =
+      Telegram::Bot::Types::ReplyKeyboardMarkup
+      .new(keyboard: [ ["/start", "/stop"], "/start", "/stop", "/start", "/stop"], one_time_keyboard: true)
+    bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+  when '/stop'
+    kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+    bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+  end
+
 	  #case message.text
-		s = message.text.mb_chars.downcase!.to_s
+			s = message.text.mb_chars.downcase!.to_s
 
-		deb << "source:\n"
-		deb << s << "\n\n"
+			deb << "source:\n"
+			deb << s << "\n\n"
 
-		obj['dataSets'].each do |elem|
-			if !s[/#{elem['regEx']}/].nil?
-				params << elem['name']
-				elem['parameters'].each do |param|
-					p "concurrence found"
-					ord_params << param
-				end
-			else
-				p "=== no concurrence"
-			end
-		end
-
-		ord_params.each do |oparam|
-			h = Hash.new
-			h["#{oparam['name']}"] = oparam['default']
-			ord2_params << h
-		end
-		p ord2_params
-		ord_params.clear
-		p params
-		p	"----------------------------"
-
-		ord2_params.map { |o2param|
-			o2param.map { |k, v|
-				obj['parameters'].each do |elem|
-					if elem['name'] == k
-						elem['values'].each { |value|
-							if !s[/#{value['regEx']}/].nil?
-								o2param[k] = value['value']
-							end
-						}
+			obj['dataSets'].each do |elem|
+				if !s[/#{elem['regEx']}/].nil?
+					params << elem['name']
+					elem['parameters'].each do |param|
+						p "concurrence found"
+						ord_params << param
 					end
-				end
-			}
-		}
-
-		#ord2_params.map do |o2param|
-		#	p o2param
-		#	o2param.map { |k, v|
-		#		obj['parameters'].each do |elem|
-		#			if elem['name'] == k
-		#				elem['values'].each { |value|
-		#					p "=_+_+_+_+_+_="
-		#					p value['regEx']
-		#					p !s[/#{value['regEx']}/].nil?
-		#					if !s[/#{value['regEx']}/].nil?
-		#						p "=====+"
-		#						p v
-		#						p value['value']
-		#						v = value['value']
-		#						p v
-		#						p "=====+"
-		#					end
-		#				}
-		#			end
-		#		end
-		#		#p k
-		#		#p v
-		#	}
-		#	p o2param
-		#end
-
-		p ord2_params
-
-		p	"----------------------------"
-		params << ord2_params
-
-		if impTovUslug['name'] == params[0]
-			impTovUslug['data'].each do |elem|
-				if ((elem['year'] == params[1][0]['year']) && (elem['quarter'] == params[1][1]['quarter']))
-					deb << elem['kind'].to_s
-					deb << "\n"
-					deb << elem['value'].to_s
-					deb << "\n"
+				else
+					p "=== no concurrence"
 				end
 			end
-		elsif
-			if vVP['name'] == params[0]
-				vVP['data'].each do |elem|
-					if elem['year'] == params[1][0]['year']
+
+			ord_params.each do |oparam|
+				h = Hash.new
+				h["#{oparam['name']}"] = oparam['default']
+				ord2_params << h
+			end
+			p ord2_params
+			ord_params.clear
+			p params
+			p	"----------------------------"
+
+			ord2_params.map { |o2param|
+				o2param.map { |k, v|
+					obj['parameters'].each do |elem|
+						if elem['name'] == k
+							elem['values'].each { |value|
+								if !s[/#{value['regEx']}/].nil?
+									o2param[k] = value['value']
+								end
+							}
+						end
+					end
+				}
+			}
+
+			p	"----------------------------"
+			params << ord2_params
+
+			if impTovUslug['name'] == params[0]
+				impTovUslug['data'].each do |elem|
+					if ((elem['year'] == params[1][0]['year']) && (elem['quarter'] == params[1][1]['quarter']))
+						deb << elem['kind'].to_s
+						deb << "\n"
 						deb << elem['value'].to_s
 						deb << "\n"
 					end
 				end
-			end
-		elsif
-			if pIA['name'] == params[0]
-				pIA['data'].each do |elem|
-					if elem['year'] == params[1][0]['year']
-            deb << elem['value'].to_s
-            deb << "\n"
+			elsif
+				if vVP['name'] == params[0]
+					vVP['data'].each do |elem|
+						if elem['year'] == params[1][0]['year']
+							deb << elem['value'].to_s
+							deb << "\n"
+						end
 					end
 				end
-			end
-		elsif
-			if aRFmpb['name'] == params[0]
-				aRFmpb['data'].each do |elem|
-					if elem['year'] == params[1][0]['year']
-            deb << elem['value'].to_s
-            deb << "\n"
+			elsif
+				if pIA['name'] == params[0]
+					pIA['data'].each do |elem|
+						if elem['year'] == params[1][0]['year']
+     		    	deb << elem['value'].to_s
+     	      	deb << "\n"
+						end
 					end
 				end
+			elsif
+				if aRFmpb['name'] == params[0]
+					aRFmpb['data'].each do |elem|
+						if elem['year'] == params[1][0]['year']
+       	    	deb << elem['value'].to_s
+            	deb << "\n"
+						end
+					end
+				end
+			else
+				deb << "some problems with this data. I DONT UNDERSTAND YOU, BABY"
+				p "some problem with this data. I DONT UNDERSTAND YOU, BABY"
 			end
-		end
 
-		deb << "\nDEBUG END"
+			deb << "\nDEBUG END"
 
-		p params
-		bot.api.send_message(chat_id: message.chat.id, text: deb)
-		deb.clear
-		ord2_params.clear
-		params.clear
+			p params
+			bot.api.send_message(chat_id: message.chat.id, text: deb)
+			deb.clear
+			ord2_params.clear
+			params.clear
+
 		#when 'A'
 		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom A")
 		#when 'B'
@@ -148,10 +136,9 @@ Telegram::Bot::Client.run(token) do |bot|
 		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom C")
 		#when 'D'
 		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom D")
-  	#when '/start'
-    #	question = 'yes, everything all right! may be one of these variants you interesting'
-    #	answers =
-    #  	Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(A B), %w(C D)], one_time_keyboard: true)
+ 		#when '/start'
+   	#	question = 'yes, everything all right! may be one of these variants you interesting'
+   	#	answers =	Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [A,B,C,D], one_time_keyboard: true)
 
     #	bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
   	#when '/stop'
