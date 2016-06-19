a0e803fa38aefa14a6a4617705be3722f10592cf
alex707 <alex.grigorenko2942@gmail.com>
2016-06-19 13:47:46 +0300

    added working with two or three parameters in request

--- kotyata.rb
+++ kotyata.rb
@@ -6,51 +6,52 @@ require 'active_support/core_ext'
 require 'json'
 require 'pp'
 
-obj = JSON.parse( IO.read("_1.json", encoding:'utf-8') )
+#obj = JSON.parse( IO.read("_1.json", encoding:'utf-8') )
+
+metaData = JSON.parse( IO.read("metaData.json", encoding:'utf-8') )
+
+vVP = 		JSON.parse( IO.read("vVP.json", encoding:'utf-8') )
+aRFmpb = 	JSON.parse( IO.read("aRFmpb.json", encoding:'utf-8') )
+pIA = 		JSON.parse( IO.read("pIA.json", encoding:'utf-8') )
+rVPnDNpSRF = JSON.parse( IO.read("rVPnDNpSRF.json", encoding:'utf-8') )
+
 impTovUslug = JSON.parse( IO.read("impTovUslug.json", encoding:'utf-8') )
-vVP = JSON.parse( IO.read("vVP.json", encoding:'utf-8') )
-pIA = JSON.parse( IO.read("pIA.json", encoding:'utf-8') )
-aRFmpb = JSON.parse( IO.read("aRFmpb.json", encoding:'utf-8') )
 
 token = '188659714:AAHB5aHkp63cp6v6_LQRbaEGXhujhQer1-U'
-deb = "DEBUG START\n"
+deb = ""
 
 params = []
+param_year = ""
+param_district = ""
+param_quarter = ""
+param_country = ""
+param_kind = ""
+
 ord_params = []
 ord2_params = []
 
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
 
-		  case message.text
-  when '/start'
-    question = 'London is a capital of which country?'
-    answers =
-      Telegram::Bot::Types::ReplyKeyboardMarkup
-      .new(keyboard: [ ["/start", "/stop"], "/start", "/stop", "/start", "/stop"], one_time_keyboard: true)
-    bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
-  when '/stop'
-    kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
-    bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
-  end
-
-	  #case message.text
 			s = message.text.mb_chars.downcase!.to_s
 
-			deb << "source:\n"
-			deb << s << "\n\n"
+			deb << "|" << "\n\n"
 
-			obj['dataSets'].each do |elem|
+			metaData['dataSets'].each do |elem|
 				if !s[/#{elem['regEx']}/].nil?
 					params << elem['name']
 					elem['parameters'].each do |param|
 						p "concurrence found"
+						p param
 						ord_params << param
 					end
+					break
 				else
 					p "=== no concurrence"
 				end
 			end
+
+			p deb
 
 			ord_params.each do |oparam|
 				h = Hash.new
@@ -64,11 +65,13 @@ Telegram::Bot::Client.run(token) do |bot|
 
 			ord2_params.map { |o2param|
 				o2param.map { |k, v|
-					obj['parameters'].each do |elem|
+					metaData['parameters'].each do |elem|
 						if elem['name'] == k
 							elem['values'].each { |value|
 								if !s[/#{value['regEx']}/].nil?
 									o2param[k] = value['value']
+									p value['value']
+									p s
 								end
 							}
 						end
@@ -79,72 +82,106 @@ Telegram::Bot::Client.run(token) do |bot|
 			p	"----------------------------"
 			params << ord2_params
 
-			if impTovUslug['name'] == params[0]
-				impTovUslug['data'].each do |elem|
-					if ((elem['year'] == params[1][0]['year']) && (elem['quarter'] == params[1][1]['quarter']))
-						deb << elem['kind'].to_s
-						deb << "\n"
-						deb << elem['value'].to_s
-						deb << "\n"
-					end
-				end
-			elsif
-				if vVP['name'] == params[0]
+			if vVP['name'] == params[0]
+
 					vVP['data'].each do |elem|
 						if elem['year'] == params[1][0]['year']
+							deb << params[0] << " " << params[1][0]['year'] << "\n"
 							deb << elem['value'].to_s
+							deb << " "
+							deb << vVP['unit']
 							deb << "\n"
 						end
 					end
-				end
+				
 			elsif
-				if pIA['name'] == params[0]
-					pIA['data'].each do |elem|
-						if elem['year'] == params[1][0]['year']
-     		    	deb << elem['value'].to_s
-     	      	deb << "\n"
-						end
-					end
-				end
-			elsif
+
 				if aRFmpb['name'] == params[0]
 					aRFmpb['data'].each do |elem|
 						if elem['year'] == params[1][0]['year']
+							deb << params[0] << " " << params[1][0]['year'] << "\n"
        	    	deb << elem['value'].to_s
+							deb << " "
+							deb << aRFmpb['unit']
             	deb << "\n"
 						end
 					end
 				end
+
+			p deb
+			elsif
+
+				if pIA['name'] == params[0]
+					pIA['data'].each do |elem|
+						if elem['year'] == params[1][0]['year']
+							deb << params[0] << " " << params[1][0]['year'] << "\n"
+     		    	deb << elem['value'].to_s
+							deb << " "
+							deb << pIA['unit']
+     	      	deb << "\n"
+						end
+					end
+				end
+
+			elsif
+
+				if rVPnDNpSRF['name'] == params[0]
+					rVPnDNpSRF['data'].each do |elem|
+						if ((elem['year'] == params[1][0]['year']) && (elem['district']) == params[1][1]['district'])
+							deb << params[0] << " " << params[1][0]['year'] << "/" << params[1][1]['district'] << "\n"
+              deb << elem['kind'].to_s
+              deb << "\n"
+              deb << elem['value'].to_s
+              deb << " "
+              deb << rVPnDNpSRF['unit']
+              deb << "\n"
+						end
+			  	end					
+				end
+
+			elsif
+
+				if vRPnDNUSA['name'] == params[0]
+					vRPnDNUSA['data'].each do |elem|
+            if ((elem['year'] == params[1][0]['year']) && (elem['country']) == params[1][1]['country'])
+              deb << params[0] << " " << params[1][0]['year'] << "/" << params[1][1]['country'] << "\n"
+              deb << elem['kind'].to_s
+              deb << "\n"
+              deb << elem['value'].to_s
+              deb << " "
+              deb << vRPnDNUSA['unit']
+              deb << "\n"
+            end
+          end
+				end
+
+			elsif
+
+				if impTovUslug['name'] == params[0]
+					impTovUslug['data'].each do |elem|
+						if ((elem['year'] == params[1][0]['year']) && (elem['quarter'] == params[1][1]['quarter']))
+							deb << params[0] << " " << params[1][0]['year'] << "/" << params[1][1]['quarter'] << "\n"
+							deb << elem['kind'].to_s
+							deb << "\n"
+							deb << elem['value'].to_s
+							deb << " "
+							deb << impTovUslug['unit']
+							deb << "\n"
+						end
+					end
+				end
+
 			else
-				deb << "some problems with this data. I DONT UNDERSTAND YOU, BABY"
+				deb << "#{metaData['noData']}"
 				p "some problem with this data. I DONT UNDERSTAND YOU, BABY"
 			end
 
-			deb << "\nDEBUG END"
-
 			p params
+			p deb
 			bot.api.send_message(chat_id: message.chat.id, text: deb)
 			deb.clear
 			ord2_params.clear
 			params.clear
 
-		#when 'A'
-		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom A")
-		#when 'B'
-		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom B")
-		#when 'C'
-		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom C")
-		#when 'D'
-		#	bot.api.send_message(chat_id: message.chat.id, text: "You are pushed buttom D")
- 		#when '/start'
-   	#	question = 'yes, everything all right! may be one of these variants you interesting'
-   	#	answers =	Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [A,B,C,D], one_time_keyboard: true)
-
-    #	bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
-  	#when '/stop'
-    #	kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
-
-    #	bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
-  	#end
 	end
 end
